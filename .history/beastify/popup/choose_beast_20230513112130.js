eedb0a53298cb508bab0d782df4fa6fbe53d354c
Bob <bboland15@gmail.com>
2023-05-13 12:39:24 -0500

    second extension tutorial

---
+++ .history/beastify/popup/choose_beast_20230513112130.js
@@ -0,0 +1,7 @@
+/**
+ * CSS to hide everything on the page,
+ * except for elements that have the "beastify-image" class.
+ */
+const hidePage = `body > :not(.beastify-image) {
+                    display: none;
+                }`;
